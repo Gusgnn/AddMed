12cb6f2d3a67c6a63fcd7fa812de535c857f8660
Gusgnn <gusgnn@gmail.com>
2025-12-01 19:57:28 -0300

    main

--- styles/home.js
+++ styles/home.js
@@ -3,26 +3,37 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    backgroundColor: "#f2f6ff",
     alignItems: "center",
+    justifyContent: "center",
     padding: 20,
-    backgroundColor: "#fff"
   },
+
   title: {
-    fontSize: 22,
-    fontWeight: "bold",
-    marginBottom: 20
+    fontSize: 26,
+    fontWeight: "700",
+    color: "#2a4d9b",
+    marginBottom: 40,
+    textAlign: "center",
   },
+
   button: {
     width: "80%",
-    backgroundColor: "#007bff",
-    padding: 15,
-    borderRadius: 8,
-    marginTop: 10
+    backgroundColor: "#3d6df2",
+    paddingVertical: 15,
+    borderRadius: 14,
+    marginBottom: 20,
+    alignItems: "center",
+    elevation: 4, // sombra no Android
+    shadowColor: "#000", // iOS
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    shadowOffset: { width: 0, height: 2 },
   },
+
   buttonText: {
-    color: "#fff",
-    textAlign: "center",
-    fontWeight: "bold"
-  }
+    fontSize: 18,
+    color: "white",
+    fontWeight: "600",
+  },
 });
